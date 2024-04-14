1a07335ed694e6020f64cde13fde1d9291c98f00
SebastianValverde911 <juan_seb.valverde@uao.edu.co>
2024-04-13 23:43:03 -0500

    back

---
+++ back/src/main/java/segurosTuPapa/seguro/controllers/ControllerGetUsers.java
@@ -0,0 +1,27 @@
+package segurosTuPapa.seguro.controllers;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import segurosTuPapa.seguro.User;
+
+
+import java.util.ArrayList;
+import java.util.List;
+
+@RestController
+@RequestMapping("/api")
+public class ControllerGetUsers {
+    private List<User> users = new ArrayList<>();
+
+    @GetMapping("/getUsers")
+    public ResponseEntity<List<User>> getUsers() {
+        users.add(new User("usuario1", "Usuario Uno", "juanseb1@hotmail.com"));
+        users.add(new User("usuario2", "Usuario Dos","juanseb2@hotmail.com"));
+        users.add(new User("usuario3", "Usuario Tres","juanse3@hotmail.com"));
+
+//      return new ResponseEntity<>(users, HttpStatus.OK);
+        return ResponseEntity.ok(users);
+    }
+}
