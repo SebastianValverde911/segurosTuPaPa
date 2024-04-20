98f79f290efc30503197db7e91adf1f3b210a8ef
SebastianValverde911 <juan_seb.valverde@uao.edu.co>
2024-04-20 04:16:42 -0500

    se realiza solo en backen el servicio de pagoExtranjero

---
+++ back/src/main/java/segurosTuPapa/seguro/repository/Icompraextranjera.java
@@ -0,0 +1,7 @@
+package segurosTuPapa.seguro.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import segurosTuPapa.seguro.entity.CompraExtranjera;
+
+public interface Icompraextranjera extends JpaRepository<CompraExtranjera, Long>{
+}
