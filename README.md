5d9f53affad23ac39e9c9b2f83eec317848e26f0
josemhc <jose_mig.hernandez@uao.edu.co>
2024-05-07 21:48:47 -0500

    Se dockerizan La API y la BD, se ajusta README

--- README.md
+++ README.md
@@ -8,6 +8,13 @@ npm install react-router-dom@6
 npm install bootstrap
 ```
 
+# Levantar Dockers para BD y API para ejecutar el proyecto
+
+Ejecutar en la terminal en el directorio de la carpeta backend:
+
+./gradlew clean build -x test
+docker-compose up 
+
 # Prueba en postman del servicio para obtener todos los usuarios
 ```
 http://localhost:8080/api/getUsers
--- back/src/main/java/segurosTuPapa/seguro/SeguroApplication.java
+++ back/src/main/java/segurosTuPapa/seguro/SeguroApplication.java
@@ -1,13 +1,21 @@
 package segurosTuPapa.seguro;
 
+import org.springframework.context.annotation.PropertySource;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@PropertySource("classpath:application.properties")
 public class SeguroApplication {
 
 	public static void main(String[] args) {
+		printEnvVariables(); // Llama al método para imprimir las variables de entorno
 		SpringApplication.run(SeguroApplication.class, args);
 	}
 
+	private static void printEnvVariables() {
+		System.out.println("DATABASE_URL: " + System.getenv("DATABASE_URL"));
+		System.out.println("DATABASE_USERNAME: " + System.getenv("DATABASE_USERNAME"));
+		System.out.println("DATABASE_PASSWORD: " + System.getenv("DATABASE_PASSWORD"));
+	}
 }
