ffe014478eb8b4581267762142b5e012b213d4ca
josemhc <jose_mig.hernandez@uao.edu.co>
2024-05-09 18:28:44 -0500

    Se ajusta el hp docker-compose.yml

--- back/src/main/java/segurosTuPapa/seguro/SeguroApplication.java
+++ back/src/main/java/segurosTuPapa/seguro/SeguroApplication.java
@@ -9,13 +9,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class SeguroApplication {
 
 	public static void main(String[] args) {
-		printEnvVariables(); // Llama al método para imprimir las variables de entorno
 		SpringApplication.run(SeguroApplication.class, args);
-	}
-
-	private static void printEnvVariables() {
-		System.out.println("DATABASE_URL: " + System.getenv("DATABASE_URL"));
-		System.out.println("DATABASE_USERNAME: " + System.getenv("DATABASE_USERNAME"));
-		System.out.println("DATABASE_PASSWORD: " + System.getenv("DATABASE_PASSWORD"));
 	}
 }
